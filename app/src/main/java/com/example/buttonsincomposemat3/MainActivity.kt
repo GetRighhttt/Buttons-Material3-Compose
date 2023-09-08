40ba7f3ce8965973b8b6ae75e01fa98f135d90b4
Stefan Bayne <stefanbusiness95@gmail.com>
2023-09-08 17:57:16 -0400

    added a preview.

--- app/src/main/java/com/example/buttonsincomposemat3/MainActivity.kt
+++ app/src/main/java/com/example/buttonsincomposemat3/MainActivity.kt
@@ -118,5 +118,79 @@ class MainActivity : ComponentActivity() {
 fun ButtonPreview() {
     ButtonsInComposeMat3Theme {
         // TODO: add in button theme to test
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            /**
+             * Will be demonstrating the various types of buttons that exist in Material 3
+             * with Jetpack Compose.
+             *
+             * With Material 3, the padding, font size, etc. of the buttons are already
+             * built in.
+             */
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                /*
+                Filled buttons are typically used when a necessary action has a big
+                impact on the user experience.
+                */
+                Button(
+                    onClick = { /*TODO*/ }
+                )
+                {
+                    Text(text = "Filled Button")
+                }
+
+                /*
+                Elevated buttons usually have a shadow behind the button, and are used if
+                we need to make the button more visible.
+                 */
+                ElevatedButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Icon( // adds Icon in button
+                        imageVector = Icons.Outlined.Add,
+                        contentDescription = "Add",
+                        modifier = Modifier.size(18.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = "Elevated Button")
+                }
+
+                /*
+                Filled Tonal Buttons are usually for actions that don't have a major impact
+                on the user experience, or for multiple buttons. Don't stand out as much.
+                 */
+                FilledTonalButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Filled Tonal Button")
+                }
+
+                /*
+                Used when you don't want much emphasis on things. Outline Buttons are meant
+                to not draw focus.
+                 */
+                OutlinedButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Back")
+                }
+
+                /*
+                Used when you want to add some text to a button. Something that is unlikely that a
+                user would click.
+                 */
+                TextButton(
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(text = "Text Button")
+                }
+            }
+        }
     }
 }
